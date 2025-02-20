96434e7727219e992e2dd131d9000f74d31477b7
BifinJ <jamesbifin@gmail.com>
2025-02-20 21:17:57 +0530

    .

--- src/components/event-paralax.tsx
+++ src/components/event-paralax.tsx
@@ -57,7 +57,7 @@ export const EventParallax = ({
   return (
     <div
       ref={ref}
-      className="h-[250vh] py-40 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
+      className="h-[210vh] md:h-[230vh] py-40 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
     >
       <Header />
       <motion.div
